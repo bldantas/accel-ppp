f7dca4fe9ac76975af90f7559f21900027691a5e
Bruno Dantas <brunodantas@redeconexaonet.com.br>
2018-03-28 01:57:48 -0300

    atualizando accel-install.sh

--- accel_install.sh
+++ accel_install.sh
@@ -2,11 +2,14 @@
 ###Instalando dependencias e Criando diretorios
 apt-get update
 apt-get -y install git vlan cmake libpcre3 libpcre3-dev libssl-dev liblua5.1 libsnmp-dev linux-headers-$(uname -r) 
-
+clear
+# Criando variaveis
 
 ACCELSRC=/usr/src/accel-ppp-code
 ACCELBUILD=/usr/src/accel-ppp
 
+# Baixando Source Accel-PPP
+echo Baixando Source Accel-PPP
 if [ ! -d "$ACCELSRC" ]; then
         git clone https://git.code.sf.net/p/accel-ppp/code /usr/src/accel-ppp-code
 else
@@ -17,15 +20,30 @@ if [ ! -d "$ACCELBUILD" ]; then
         mkdir /usr/src/accel-ppp
 fi
 
+echo Compilando...
+
 rm -rf /usr/src/accel-ppp/*
 cd /usr/src/accel-ppp/
 cmake -DRADIUS=TRUE -DSHAPER=TRUE -DBUILD_IPOE_DRIVER=TRUE -DBUILD_VLAN_MON_DRIVER=TRUE -DKDIR=/usr/src/linux-headers-`uname -r` -DNETSNMP=TRUE -D LUA=TRUE -DLOG_PGSQL=FALSE /usr/src/accel-ppp-code/
 
+clear
+
 make
 make install
 mkdir -p /lib/modules/$(uname -r)/kernel/extra
+
+clear
+
+# Copiando Drives
 cp ./drivers/ipoe/driver/ipoe.ko /lib/modules/$(uname -r)/kernel/extra
 cp ./drivers/vlan_mon/driver/vlan_mon.ko /lib/modules/$(uname -r)/kernel/extra
+
+clear
+
+# Adicionando Modulos
+
+echo Copiando Modulos
+
 depmod -a
 modprobe ipoe
 modprobe vlan_mon
@@ -33,6 +51,7 @@ echo "8021q" >> /etc/modules
 echo "ipoe" >> /etc/modules
 echo "vlan_mon" >> /etc/modules
 clear
+
 echo Copiando arquivos de configuracoes
 cd /accel-ppp/
 cp -r dictionary /usr/src/accel-ppp/accel-pppd/
